641a43349c8d05afabff99453043dd59ef1dd547
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-21 08:36:57 +0300

    Create Django_Database_Funtions.py

---
+++ Django_Database_Funtions.py
@@ -0,0 +1,397 @@
+Django provides a variety of database functions that allow you to perform complex queries and calculations on your database data.
+
+Here are some of the most commonly used database functions in Django:
+
+
+
+Aggregate functions:
+  These functions are used to perform calculations on a set of values and return a single result. Examples include Count, Sum, Avg, Max, and Min.
+
+Date and time functions:
+  These functions allow you to perform operations on date and time fields. Examples include Extract, Trunc, Date, and Time.
+
+String functions: 
+  These functions allow you to perform operations on text fields. Examples include Concat, Upper, Lower, and Substring.
+
+Mathematical functions: 
+  These functions allow you to perform mathematical calculations on numeric fields. Examples include Abs, Ceil, Floor, and Round.
+
+Conditional expressions:
+  These functions allow you to perform conditional logic in queries. Examples include Case, When, and Coalesce.
+
+Window functions: 
+  These functions allow you to perform calculations on a "window" of rows in a result set. Examples include Row_number, Rank, and Dense_rank.
+
+JSON functions: 
+  These functions allow you to work with JSON data stored in the database. Examples include Json_extract_path and Jsonb_set.
+
+
+
+
+  
+  STRING/ TEXT FUNCTIONS
+  ******************************************************************************************************************************************
+  These functions allow you to perform operations on text fields. Examples include Concat, Upper, Lower, and Substring.
+  
+  
+  
+ Concat: This function concatenates two or more string fields together.
+
+Left: This function returns a specified number of characters from the beginning (left) of a string field.
+
+Length: This function returns the length of a string field.
+
+Lower: This function converts a string field to lowercase.
+
+LPad: This function pads a string field on the left (beginning) with a specified character until it reaches a specified length.
+
+LTrim: This function removes any whitespace characters from the beginning (left) of a string field.
+
+MD5: This function returns the MD5 hash of a string field.
+
+Ord: This function returns the ASCII code for the first character of a string field.
+
+Repeat: This function repeats a string field a specified number of times.
+
+Replace: This function replaces all occurrences of a specified substring within a string field with another specified substring.
+
+Reverse: This function reverses the characters in a string field.
+
+Right: This function returns a specified number of characters from the end (right) of a string field.
+
+RPad: This function pads a string field on the right (end) with a specified character until it reaches a specified length.
+
+RTrim: This function removes any whitespace characters from the end (right) of a string field.
+
+StrIndex: This function returns the position of the first occurrence of a specified substring within a string field.
+
+Substr: This function returns a specified substring from within a string field.
+
+Trim: This function removes any whitespace characters from the beginning and end of a string field.
+
+Upper: This function converts a string field to uppercase.
+
+In addition, you also listed some hash functions (SHA1, SHA224, SHA256, SHA384, and SHA512). 
+These are cryptographic hash functions that take an input string and return a fixed-length hash value.
+They are used for security purposes, such as generating unique identifiers for passwords or other sensitive data.
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+Sure! Here are some examples of text-related database functions in Django:
+
+Concat:
+  This function allows you to concatenate two or more string fields. Here's an example:
+This would add a full_name field to each instance of MyModel, which concatenates the first_name and last_name fields.
+  
+  
+  from django.db.models import Concat, F
+
+MyModel.objects.annotate(full_name=Concat('first_name', F('last_name'), output_field=CharField()))
+
+
+
+
+Upper and Lower:
+  These functions convert a string field to uppercase or lowercase, respectively. Here's an example:
+This would add an upper_name and a lower_name field to each instance of MyModel, which convert the name field to uppercase and lowercase, respectively.
+
+from django.db.models import Upper, Lower
+
+MyModel.objects.annotate(upper_name=Upper('name'), lower_name=Lower('name'))
+
+
+
+
+Substring: 
+  This function allows you to extract a substring from a string field. Here's an example:
+This would add a substring field to each instance of MyModel, which extracts a substring of length 3 starting from the second character of the name field.
+
+from django.db.models import Substr
+
+MyModel.objects.annotate(substring=Substr('name', 1, 3))
+
+
+
+
+Length:
+  This function returns the length of a string field. Here's an example:
+This would add a name_length field to each instance of MyModel, which contains the length of the name field.
+
+from django.db.models import Length
+
+MyModel.objects.annotate(name_length=Length('name'))
+
+
+
+
+
+Replace:
+  This function allows you to replace a substring within a string field with another string. Here's an example
+This would add a new_name field to each instance of MyModel, which replaces all occurrences of the substring "Old" within the name field with the string "New".
+
+from django.db.models import Replace
+
+MyModel.objects.annotate(new_name=Replace('name', 'Old', 'New'))
+
+
+
+
+Trim: 
+  This function allows you to remove whitespace from the beginning and end of a string field. Here's an example:
+This would add a trimmed_name field to each instance of MyModel, which contains the name field with leading and trailing whitespace removed.
+
+
+from django.db.models import Trim
+
+MyModel.objects.annotate(trimmed_name=Trim('name'))
+
+
+
+
+Left and Right:
+  These functions allow you to extract a specified number of characters from the beginning or end of a string field, respectively. Here's an example:
+This would add a left_name and a right_name field to each instance of MyModel, which contain the first three and last three characters of the name field, respectively.
+
+
+from django.db.models import Left, Right
+
+MyModel.objects.annotate(left_name=Left('name', 3), right_name=Right('name', 3))
+
+
+RegexpReplace: 
+  This function allows you to replace substrings within a string field based on a regular expression pattern. Here's an example:
+This would add a new_name field to each instance of MyModel, which replaces all vowels within the name field with a hyphen ("-").
+
+
+from django.db.models import RegexpReplace
+
+MyModel.objects.annotate(new_name=RegexpReplace('name', r'[aeiou]', '-'))
+
+
+
+
+
+Left:
+  This function returns a specified number of characters from the beginning (left) of a string field.
+  This would add a left_three_chars field to each instance of MyModel, which contains the first three characters of the name field.
+
+from django.db.models.functions import Left
+
+MyModel.objects.annotate(left_three_chars=Left('name', 3))
+
+
+
+
+Length: This function returns the length of a string field.
+  This would add a name_length field to each instance of MyModel, which contains the length of the name field.
+
+
+  from django.db.models.functions import Length
+
+MyModel.objects.annotate(name_length=Length('name'))
+
+
+
+Lower: This function converts a string field to lowercase.
+This would add a lower_name field to each instance of MyModel, which contains the name field converted to lowercase.
+
+from django.db.models.functions import Lower
+
+MyModel.objects.annotate(lower_name=Lower('name'))
+
+
+
+LPad: 
+  This function pads a string field on the left (beginning) with a specified character until it reaches a specified length.
+
+This would add a padded_name field to each instance of MyModel, 
+which contains the name field padded on the left with 0 characters until it reaches a length of 10 characters.
+
+from django.db.models.functions import LPad
+
+MyModel.objects.annotate(padded_name=LPad('name', 10, '0'))
+
+
+
+
+
+LTrim:
+  This function removes any whitespace characters from the beginning (left) of a string field.
+
+This would add a trimmed_name field to each instance of MyModel, which contains the name field with any whitespace characters removed from the beginning.
+
+from django.db.models.functions import LTrim
+
+MyModel.objects.annotate(trimmed_name=LTrim('name'))
+
+
+
+
+MD5:
+  This function returns the MD5 hash of a string field.
+This would add a name_hash field to each instance of MyModel, which contains the MD5 hash of the name field.
+
+
+from django.db.models.functions import MD5
+
+MyModel.objects.annotate(name_hash=MD5('name'))
+
+
+
+
+Ord:
+  This function returns the ASCII code for the first character of a string field.
+This would add a first_char_code field to each instance of MyModel, which contains the ASCII code for the first character of the name field.
+
+
+from django.db.models.functions import Ord
+
+MyModel.objects.annotate(first_char_code=Ord('name'[0]))
+
+
+
+
+Repeat: 
+  This function repeats a string field a specified number of times.
+This would add a repeated_name field to each instance of MyModel, which contains the name field repeated three times.
+
+from django.db.models.functions import Repeat
+
+MyModel.objects.annotate(repeated_name=Repeat('name', 3))
+
+
+
+Replace:
+  This function replaces all occurrences of a specified substring within a string field with another specified substring.
+
+from django.db.models.functions import Replace
+
+MyModel.objects.annotate(replaced_name=Replace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
