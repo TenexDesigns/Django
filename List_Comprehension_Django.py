990ed5441d816f16277afc7df4898c13aa79c350
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-13 13:20:30 +0300

    Create List_Comprehension_Django.py

---
+++ List_Comprehension_Django.py
@@ -0,0 +1,174 @@
+
+In Django, you can use list comprehensions to perform operations on lists or querysets in a concise and readable manner. 
+List comprehensions allow you to iterate over a sequence and apply conditions or transformations to each element, creating a new list as a result. 
+Here are a few examples of how you can use list comprehensions in Django:
+
+
+1.Transforming QuerySets:
+
+# Retrieve a list of usernames from User objects
+usernames = [user.username for user in User.objects.all()]
+
+# Retrieve a list of product names in uppercase
+uppercase_names = [product.name.upper() for product in Product.objects.all()]
+
+
+or
+
+sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+
+
+
+2.Filtering QuerySets:
+  
+In the examples above, the list comprehensions filter the queryset based on certain conditions and create new lists containing only the desired objects
+
+
+
+
+# Retrieve all active users
+active_users = [user for user in User.objects.all() if user.is_active]
+
+# Retrieve all products with a price greater than 100
+expensive_products = [product for product in Product.objects.all() if product.price > 100]
+
+
+
+
+
+List comprehensions can be powerful tools to simplify and optimize your code when working with lists or querysets in Django.
+They allow you to perform filtering, transformation, and combination operations in a concise and readable manner.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+..
